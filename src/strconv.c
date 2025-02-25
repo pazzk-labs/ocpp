8085582a84e54c1d14055845d823ca37c8de511a
Kyunghwan Kwon <k@pazzk.net>
2025-02-26 01:40:00 +0900

    Add ocpp_stringify_measurand function

--- src/strconv.c
+++ src/strconv.c
@@ -281,3 +281,34 @@ const char *ocpp_stringify_unit(ocpp_measure_unit_t unit)
 
 	return tbl[unit];
 }
+
+const char *ocpp_stringify_measurand(char *buf, size_t bufsize,
+		ocpp_measurand_t measurand)
+{
+	const char *tbl[] = {
+		"Current.Export",
+		"Current.Import",
+		"Current.Offered",
+		"Energy.Active.Export.Register",
+		"Energy.Active.Import.Register",
+		"Energy.Reactive.Export.Register",
+		"Energy.Reactive.Import.Register",
+		"Energy.Active.Export.Interval",
+		"Energy.Active.Import.Interval",
+		"Energy.Reactive.Export.Interval",
+		"Energy.Reactive.Import.Interval",
+		"Frequency",
+		"Power.Active.Export",
+		"Power.Active.Import",
+		"Power.Factor",
+		"Power.Offered",
+		"Power.Reactive.Export",
+		"Power.Reactive.Import",
+		"RPM",
+		"SoC",
+		"Temperature",
+		"Voltage",
+	};
+
+	return tbl[__builtin_ctz(measurand)];
+}
