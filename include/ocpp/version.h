75078cda48e1133f6e56f7befc447948eee5fdaa
Kyunghwan Kwon <k@pazzk.net>
2025-02-26 00:26:17 +0900

    ci: add workflow to check version before release

---
+++ include/ocpp/version.h
@@ -0,0 +1,23 @@
+/*
+ * SPDX-FileCopyrightText: 2024 권경환 Kyunghwan Kwon <k@libmcu.org>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef LIBMCU_OCPP_VERSION_H
+#define LIBMCU_OCPP_VERSION_H
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+#define OCPP_VERSION_STRING	"0.0.1"
+#define OCPP_VERSION_MAJOR	0
+#define OCPP_VERSION_MINOR	0
+#define OCPP_VERSION_BUILD	1
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif /* LIBMCU_OCPP_VERSION_H */
